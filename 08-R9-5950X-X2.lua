7e165431a534fc2e99f8d5be7f490347d28101d1
NopChillz <82336260+NopChillz@users.noreply.github.com>
2023-12-07 12:07:17 +0700

    Update 08-R9-5950X-X2.lua

--- 08-R9-5950X-X2.lua
+++ 08-R9-5950X-X2.lua
@@ -1,93 +1,22 @@
-wait(5)
-_G.KaitunConfig = {
-	["Auto Boost FPS"] = true,
-	["Skip Level Farm"] = true,
-	["Fast Attack Mode"] = "Fast Attack", -- "Fast Attack" , "More Then old Fast Attack" 
-	["Hop If Can't Kill Player"] = true,
-	["Block All Hop"] = true,
-	
-	["Start Kaitun"] = true,
-	-- World 1
-	["Auto Pole"] = true, -- จะตีเเค่ถ้ามันเกิดไม่ได้ตีจนกว่าจะได้
-	["Auto Saber"] = true,
-	["Auto Second World"] = true,
-	-- World 2
-	["Auto Rengoku"] = true,
-	["Auto Quest Flower"] = true,
-	["Auto V3 Race"] = true,
-	["Auto Bartilo"] = true,
-	["Auto Cursed Captain"] = true,
-	["Auto Darkbeard"] = true,
-	["Auto Factory"] = true,
-	["Auto Third World"] = true,
-	["Skip Get Item Guitar"] = true, -- จะไม่ หาของทำ soul guiter ในโลก 2 เบบ หาจนกว่าจะได้ will not find item until get all item for do soul guiter ( open recommend เเนะนำให้เปิด )
-	["Allise Fruit"] = {"Dragon-Dragon","Dough-Dough","Leopard-Leopard","Mammoth-Mammoth"}, -- จะไม่ใช้ผลพวกนี้ในการเปิดประตูไปโลก3
-	-- World 3
-	["Auto Hallow Scythe"] = true,
-	["Auto Buddy Sword"] = true,
-	["Auto Dough King"] = true,
-	["Auto Spikey Trident"] = true,
-	["Auto Tushita"] = true,
-	["Auto Elite Hunter"] = true,
-	["Auto rip Indra"] = true,
-	["Auto Yama"] = true,
-	["Auto Canvander"] = true,
-	["Auto Soul Guitar"] = true, 
-	["Auto Rainbow Haki"] = true,
-	["Auto Cursed Dual Katana"] = true,
-	
-	-- Fighting Style 
-	
-	["Auto God Human"] = true,
-	["Auto Superhuman"] = true,
-	["Auto Death Step"] = true,
-	["Auto Sharkman Karate"] = true,
-	["Auto Electric Claw"] = true,
-	["Auto Dargon Talon"] = true,
-	
-	["Auto Mastery Devil Fruit"] = true,
-	["Skill Setting"] = { -- ถ้าไม่ใส่จะใช้ mode auto
-		-- ["Z"] = 0.1,
-		-- ["X"] = 0.1,
-		-- ["C"] = 0.1,
-		-- ["V"] = 0.1, -- อันไหนไม่เอาลบออกไปเลย
-	},
-	["Auto Mastery Sword"] = true,
-	["Sword Rarity Select"] = {"Mythical","Legendary"}, -- Common , Uncommon,Rare,Legendary,Mythical
+if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
 
-	["Select Redeem Code Level"] = 1,
+_G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 300วิ
+_G.Bypass_Tp = true -- วาปฟาร์ม
 
-	-- Raids
-
-	["Select Rate Fruit Rais"] = 1000000, -- if fruit price less u rate then we use it to auto raids
-	["Limit Raids Fragments"] = 100000,
-
-	-- Devil Fruit
-
-	["Select Main Devil Fruit Sniper"] = {"Dragon-Dragon","Dough-Dough","Leopard-Leopard","Mammoth-Mammoth"}, -- ผลหลักที่จะกินเเทนผลรอง
-	["Select Sub Devil Fruit Sinper"] = {"Buddha-Buddha","Ice-Ice","Dark-Dark","Light-Light"}, -- ผลรองจะกินไว้ก่อนเเล้วพอผลหลักมีก้จะเปลียนไปกินผิดหลัก
-	["Allow Eat Fruit In Inventory"] = true,
-	["Start Sniper"] = true,
-
-	-- RAM
-
-	["RAM Port"] = 7963,
-	["RAM Password"] = "",
-	["Auto Set Description"] = false,
-
-	-- Webhook
-
-	["Start Webhook"] = false,
-	["Link Webhook"] = "",
-	["Mode Webhook"] = "Send On Level Max", -- "Send Every 10 min","Send On Level Max","Send On Level Max And Every 10 min"
-
-	-- CPU
-	
-	["Select Lock Fps"] = 15,
-	["Lock Fps Now"] = true,
-	["White Screen"] = true
+_G.Select_Fruit = {
+    ['Main'] = {'Dough-Dough','Leopard-Leopard'}, -- เลือกผลปีศาจหลัก
+    ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Light-Light','Dragon-Dragon','Mammoth-Mammoth'} -- เลือกผลปีศาจ
+}
+_G.Mastery_Farm = { -- ฟามมาสเตอรี่
+    ['Melee'] = true,
+    ['Devil Fruit'] = true,
+    ['Sword'] = true,
+    ['Gun'] = false
+}
+_G.Race_Evo = {
+    ['Level'] = {1,2,3}, -- อีโวเผ่าจากขั้น0 ไปถึงขั้น3
+    ['Enabled'] = true
 }
 
-_G.Key = "KCVF0-YQS5E-E44CG"
-_G.DiscordId = "904357258190811178"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))();
+script_key="PEDDznDXwNYinkXLsrSAfHVOgoVcsCcE";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
