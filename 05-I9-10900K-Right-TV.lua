bd43737ad1d3632fe3c0b600297d73c803d8bba5
NopChillz <82336260+NopChillz@users.noreply.github.com>
2023-12-16 12:19:44 +0700

    Update 05-I9-10900K-Right-TV.lua

--- 05-I9-10900K-Right-TV.lua
+++ 05-I9-10900K-Right-TV.lua
@@ -4,7 +4,7 @@ _G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 
 _G.Bypass_Tp = true -- วาปฟาร์ม
 
 _G.Select_Fruit = {
-    ['Main'] = {'Dough-Dough','Leopard-Leopard'}, -- เลือกผลปีศาจหลัก
+    ['Main'] = {'Dough-Dough','Leopard-Leopard','Kitsune-Kitsune'}, -- เลือกผลปีศาจหลัก
     ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Light-Light','Dragon-Dragon','Mammoth-Mammoth'} -- เลือกผลปีศาจ
 }
 _G.Mastery_Farm = { -- ฟามมาสเตอรี่
