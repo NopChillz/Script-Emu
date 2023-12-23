69196b4697b8962d149f505e7c3bca536ae77df2
NopChillz <82336260+NopChillz@users.noreply.github.com>
2023-12-23 21:27:39 +0700

    Update 09-I9-10900K.lua

--- 09-I9-10900K.lua
+++ 09-I9-10900K.lua
@@ -1,33 +1,22 @@
-wait(5)
-_G.RoyxSpecialMode = "Hell Factory";
-_G["External Config"] = {
-    ["Kai Gems"] = true, -- true / false
-    ["Kai Story"] = false, -- true / false
-    ["Kai Battle Pass"] = false, -- true / false
-    ["Kai Infinity Castle"] = false, -- true / false
-    ["Auto Set Resource"] = true, -- true / false
-    ["Select End Game Mode"] = "Instant Leave", -- "Instant Leave" / "Auto Sell"
-    ["End Wave"] = 25, -- number
-    ["Use World Jumper"] = true, -- true / false
-    ["Auto Clam Battle Pass"] = true, -- true / false
-    ["Maximum Added Gems"] = 60000, -- number
-    ["Close Roblox When Maximum Added Gems"] = false, -- true / false
-    ["Using Limit Farming"] = true, -- true / false
-    ["Stop Farming When The Limit Is Reached"] = false, -- true / false
-    ["Send Webhook When The Limit Is Reached"] = false, -- true / false
-    ["Webhook Url"] = "", -- string
-    ["RAM Port"] = "7963", -- string
-    ["RAM Password"] = "nongnop001", -- string
-    ["RAM Private Server Url"] = "", -- string
-    ["RAM Auto Description"] = true, -- true / false
-    ["RAM Auto Private Server"] = false, -- true / false
-    ["RAM Auto Exit"] = false, -- true / false
-    ["Fps Limit"] = 15, -- number
-    ["Lock Fps"] = true, -- true / false
-    ["Release Fps When Rejoin"] = false, -- true / false
-    ["Select Unit"] = {"Slot 1", "Slot 2", "Slot 3"}, -- table  {"Slot 1", "Slot 2", "Slot 3", "Slot 4", "Slot 5", "Slot 6"}
-};
+if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
 
-_G.Key = "932HO-Z7XKB-W9J0Y"
-_G.DiscordId = "726490576790487042"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Natsuhanaki/Royx_PC/main/loader.lua"))();
+_G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 300วิ
+_G.Bypass_Tp = true -- วาปฟาร์ม
+
+_G.Select_Fruit = {
+    ['Main'] = {'Dough-Dough','Leopard-Leopard','Kitsune-Kitsune'}, -- เลือกผลปีศาจหลัก
+    ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Light-Light','Dragon-Dragon','Mammoth-Mammoth'} -- เลือกผลปีศาจ
+}
+_G.Mastery_Farm = { -- ฟามมาสเตอรี่
+    ['Melee'] = true,
+    ['Devil Fruit'] = true,
+    ['Sword'] = true,
+    ['Gun'] = false
+}
+_G.Race_Evo = {
+    ['Level'] = {1,2,3}, -- อีโวเผ่าจากขั้น0 ไปถึงขั้น3
+    ['Enabled'] = true
+}
+
+script_key="HbATtiVQnthOAnVCXsdmfLwnNKbcCQaI";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
