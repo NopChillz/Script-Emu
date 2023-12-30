45ecc3b392ec937376da2b807f73e8b4f3211a49
NopChillz <82336260+NopChillz@users.noreply.github.com>
2023-12-30 22:03:59 +0700

    Add files via upload

---
+++ 01-Xeon - Switch Hub.lua
@@ -0,0 +1,22 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
+
+_G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 300วิ
+_G.Bypass_Tp = false -- วาปฟาร์ม
+
+_G.Select_Fruit = {
+    ['Main'] = {'Dough-Dough','Leopard-Leopard','Kitsune-Kitsune'}, -- เลือกผลปีศาจหลัก
+    ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Light-Light','Dragon-Dragon','Mammoth-Mammoth'} -- เลือกผลปีศาจ
+}
+_G.Mastery_Farm = { -- ฟามมาสเตอรี่
+    ['Melee'] = true,
+    ['Devil Fruit'] = true,
+    ['Sword'] = true,
+    ['Gun'] = false
+}
+_G.Race_Evo = {
+    ['Level'] = {1,2,3}, -- อีโวเผ่าจากขั้น0 ไปถึงขั้น3
+    ['Enabled'] = true
+}
+
+script_key="FkeOeJhcaYvJYNqKRNZkatBHJFkLoFLS";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
