640cec17437d482b679f0cd9c6a9f80b956ae260
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-01 10:57:20 +0700

    Update Check Level.lua

--- Check Level.lua
+++ Check Level.lua
@@ -1,13 +1,7 @@
-print("Scrpint Check Level V2 Work !!")
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer
-local LevelData = LocalPlayer.Data.Level
-local OldLevel = LevelData -- เก็บเลเวลเก่า
-
 -- Function Teleport 
 local PlaceID = game.PlaceId
-local Run = game:GetService("RunService")
-local SetSkill = nil 
+local Run = game:GetService("RunService") ;
+local SetSkill = nil ; 
 local AllIDs = {}
 local foundAnything = ""
 local actualHour = os.date("!*t").hour
@@ -19,7 +13,6 @@ if not File then
     table.insert(AllIDs, actualHour)
     writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
 end
-
 function TPReturner(kopkuy)
     local Site;
     if foundAnything == "" then
@@ -27,63 +20,51 @@ function TPReturner(kopkuy)
     else
         Site = game.HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. PlaceID .. '/servers/Public?sortOrder=Asc&limit=100&cursor=' .. foundAnything))
     end
-    
-    if Site and Site.data then  -- เพิ่มการตรวจสอบ Site และ Site.data
-        local ID = ""
-        local num = 0;
-        for i,v in pairs(Site.data) do
-            local Possible = true
-            ID = tostring(v.id)
-            if tonumber(v.maxPlayers) > tonumber(v.playing) then
-                for _,Existing in pairs(AllIDs) do
-                    if num ~= 0 then
-                        if ID == tostring(Existing) then
-                            Possible = false
-                        end
-                    else
-                        if tonumber(actualHour) ~= tonumber(Existing) then
-                            local delFile = pcall(function()
-                                delfile("NotSameServers.json")
-                                AllIDs = {}
-                                table.insert(AllIDs, actualHour)
-                            end)
-                        end
+    local ID = ""
+    if Site.nextPageCursor and Site.nextPageCursor ~= "null" and Site.nextPageCursor ~= nil then
+        foundAnything = Site.nextPageCursor
+    end
+    local num = 0;
+    for i,v in pairs(Site.data) do
+        local Possible = true
+        ID = tostring(v.id)
+        if tonumber(v.maxPlayers) > tonumber(v.playing) then
+            for _,Existing in pairs(AllIDs) do
+                if num ~= 0 then
+                    if ID == tostring(Existing) then
+                        Possible = false
                     end
-                    num = num + 1
+                else
+                    if tonumber(actualHour) ~= tonumber(Existing) then
+                        local delFile = pcall(function()
+                            delfile("NotSameServers.json")
+                            AllIDs = {}
+                            table.insert(AllIDs, actualHour)
+                        end)
+                    end
                 end
-                if Possible == true then
-                    table.insert(AllIDs, ID)
+                num = num + 1
+            end
+            if Possible == true then
+                table.insert(AllIDs, ID)
+                wait()
+                pcall(function()
+                    writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
                     wait()
-                    pcall(function()
-                        writefile("NotSameServers.json", game:GetService('HttpService'):JSONEncode(AllIDs))
-                        wait()
-                        if kopkuy  then
-                            game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
-                        end
-                    end)
-                    wait(4)
-                end
+                    if kopkuy  then
+                        game:GetService("TeleportService"):TeleportToPlaceInstance(PlaceID, ID, game.Players.LocalPlayer)
+                    end
+                end)
+                wait(4)
             end
         end
     end
 end
 
-function CheckLevel()
-    while true do
-        wait(30) -- รอ 3 นาที
-
-        local CurrentLevel = LevelData -- เลเวลปัจจุบัน
-
-        if CurrentLevel == OldLevel then
-            Teleport(true)
-            print("Hop Server")
-        else
-            print("Let's go farm")
-        end
-
-        OldLevel = CurrentLevel -- อัปเดตเลเวลเก่าเป็นเลเวลปัจจุบัน
+function Teleport(v)
+    while true do wait()
+        pcall(function()
+            TPReturner(v)
+        end)
     end
 end
-
--- เรียกใช้งานฟังชั่น CheckLevel
-CheckLevel()
