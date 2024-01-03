e8b397a7021e4f5bb8e501d7cf4ca944dcdc74fc
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-04 00:10:52 +0700

    Update LogDes - TEST.lua

--- LogDes - TEST.lua
+++ LogDes - TEST.lua
@@ -1,9 +1,7 @@
-while true do
     local plr = game.Players.LocalPlayer 
 
     local oldCFrame = plr.Character.HumanoidRootPart.CFrame.Position
-    wait(10)
+    wait(2)
     local currentCFrame = plr.Character.HumanoidRootPart.CFrame.Position
 	print(oldCFrame)
 	print(currentCFrame)
-end
