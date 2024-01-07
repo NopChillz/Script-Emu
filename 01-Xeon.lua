dc775144088d304bbe6c350468703fc93d697996
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-07 10:45:19 +0700

    Update 01-Xeon.lua

--- 01-Xeon.lua
+++ 01-Xeon.lua
@@ -18,5 +18,5 @@ _G.Race_Evo = {
     ['Enabled'] = true
 }
 
-script_key="rKnuubFGRtrAfxRBixpxqBwDBItHkPfM";
+script_key="xKIfwnfiNHzwafgdSoIFCGxwkKjRaxPC";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
