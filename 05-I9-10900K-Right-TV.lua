2c6b6cd99425c2ee1c019ebf42b3804016c9eff2
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-07 10:52:13 +0700

    Update 05-I9-10900K-Right-TV.lua

--- 05-I9-10900K-Right-TV.lua
+++ 05-I9-10900K-Right-TV.lua
@@ -18,5 +18,5 @@ _G.Race_Evo = {
     ['Enabled'] = true
 }
 
-script_key="HueDOwlmQlXJMaKJTSxDIocVNuTgICjZ";
+script_key="GPPYmJtJDVXXIsjWIgWQobPascFRjFnX";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
