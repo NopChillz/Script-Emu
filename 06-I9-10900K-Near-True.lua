6f866fb3e243be7030f03a152d300ef2f06826a9
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-07 08:04:21 +0700

    Update 06-I9-10900K-Near-True.lua

--- 06-I9-10900K-Near-True.lua
+++ 06-I9-10900K-Near-True.lua
@@ -18,5 +18,5 @@ _G.Race_Evo = {
     ['Enabled'] = true
 }
 
-script_key="fSewqVCiDcWlixBJdewBuycEQkeAtOZr";
+script_key="UBPtMFFHxVatokudPSTvftJvJfxPqJRe";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
