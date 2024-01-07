acb8a818e2d8c7c982d1cba51b9097a8b128a18e
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-07 10:51:51 +0700

    Update 02-R9-5950X-X1.lua

--- 02-R9-5950X-X1.lua
+++ 02-R9-5950X-X1.lua
@@ -18,5 +18,5 @@ _G.Race_Evo = {
     ['Enabled'] = true
 }
 
-script_key="duzyYfdtCVWkWSTHQaMdkufWZtWrWcDb";
+script_key="TXzDcFOBqwZQfPbKSOVlUNrjjVtmwDMn";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
