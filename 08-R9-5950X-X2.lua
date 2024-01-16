b57334b44f0f899a352768d13cea448ce035ad72
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-16 08:28:49 +0700

    Update 08-R9-5950X-X2.lua

--- 08-R9-5950X-X2.lua
+++ 08-R9-5950X-X2.lua
@@ -1,22 +1,86 @@
-if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
+wait(5)
+shared.Team = "Pirates"
+getgenv().Configs = {
+    FpsBoost = true,
+    SkipFarm = true,
+    HopIfCantKill = true,
+    BlockAllHop = false,
 
-_G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 300วิ
-_G.Bypass_Tp = true -- วาปฟาร์ม
+    FastAttack = true,
+    NewFastAttack = true,
+    NoAttackAnimation = true,
+    
+    StartKaitun = true,
+    -- World 1
+    AutoPole = true, -- จะตีเเค่ถ้ามันเกิดไม่ได้ตีจนกว่าจะได้
+    AutoSaber = false,
+    
+    AutoSecondSea = true,
+    -- World 2
+    AutoRengoku = true,
+    AutoQuestFlower = true,
+    AutoRaceV3 = false,
+    AutoBartiloQuest = true,
+    AutoCursedCaptain = true,
+    AutoDarkbeard = true,
+    AutoFactory = true,
+    AutoThirdSea = true,
+    SkipGetItemGuitar = true, -- จะไม่ หาของทำ soul guiter ในโลก 2 เบบ หาจนกว่าจะได้ will not find item until get all item for do soul guiter ( open recommend เเนะนำให้เปิด )
+    AlliesFruit = {"Dough-Dough","Leopard-Leopard","Kitsune-Kitsune"}, -- จะไม่ใช้ผลพวกนี้ในการเปิดประตูไปโลก3
+    -- World 3
+    AutoHallowScythe = true,
+    AutoBuddySword = true,
+    AutoDoughKing = true,
+    AutoSpikeyTrident = true,
+    AutoTushita = true,
+    AutoEliteHunter = true,
+    AutoDarkDagger = true,
+    AutoYama = true,
+    AutoCanvander = true,
+    AutoSoulGuitar = true, 
+    AutoRainbowHaki = true,
+    AutoCursedDualKatana = true,
+    
+    -- Fighting Style 
+    
+    AutoGodHuman = true,
+    AutoSuperhuman = true,
+    AutoDeathStep = true,
+    AutoSharkmanKarate = true,
+    AutoElectricClaw = true,
+    AutoDargonTalon = true,
+    
+    AutoDFMastery = true,
+    SettingsSkill = { -- ถ้าไม่ใส่จะใช้ mode auto
+        -- ["Z"] = 0.1,
+        -- ["X"] = 0.1,
+        -- ["C"] = 0.1,
+        -- ["V"] = 0.1, -- อันไหนไม่เอาลบออกไปเลย
+    },
+    AutoSwordMastery = true,
+    SelectRaritySword = {"Mythical","Legendary"}, -- Common , Uncommon,Rare,Legendary,Mythical
+    
+    SelectRedeemCodeLevel = 1,
+    
+    -- Raids
+    
+    SelectRateFruitRaid = 1000000, -- if fruit price less u rate then we use it to auto raids
+    LimitFragmentsRaids = 200000,
+    
+    -- Devil Fruit
+        
+    SelectMainDF = {}, -- ผลหลักที่จะกินเเทนผลรอง
+    SelectSubDF = {"Dark-Dark","Human-Human: Buddha","Light-Light","Dragon-Dragon","Mammoth-Mammoth","Dough-Dough","Leopard-Leopard","Kitsune-Kitsune"}, -- ผลรองจะกินไว้ก่อนเเล้วพอผลหลักมีก้จะเปลียนไปกินผิดหลัก
+    AllowEatDFInventory = true,
+    StartSniper = true,
+    
+    -- CPU
+    
+    LockFPS = 15,
+    LockFPSNow = true,
+    WhiteScreen = true
+}
 
-_G.Select_Fruit = {
-    ['Main'] = {''}, -- เลือกผลปีศาจหลัก
-    ['Select Fruit'] = {'Dark-Dark','Human-Human: Buddha','Light-Light','Dragon-Dragon','Mammoth-Mammoth','Kitsune-Kitsune','Dough-Dough','Leopard-Leopard'} -- เลือกผลปีศาจ
-}
-_G.Mastery_Farm = { -- ฟามมาสเตอรี่
-    ['Melee'] = true,
-    ['Devil Fruit'] = true,
-    ['Sword'] = true,
-    ['Gun'] = false
-}
-_G.Race_Evo = {
-    ['Level'] = {1,2,3}, -- อีโวเผ่าจากขั้น0 ไปถึงขั้น3
-    ['Enabled'] = true
-}
-
-script_key="OtNoTVUFXqqnRdIOrLQERSqEjyvEylzB";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
+_G.Key = "GU75P-4NW1H-A9PPN"
+_G.DiscordId = "1016006967149482054"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/NightsTimeZ/RoyryX/main/Loader.lua"))()
