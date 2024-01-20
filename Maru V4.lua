49e6dc5f9dc227ad9df4a52860501cd2935a5328
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-20 17:19:27 +0700

    Add files via upload

---
+++ Maru V4.lua
@@ -0,0 +1,4 @@
+getgenv().Key = "MARU-0D4F-3GS36-UP83-PRZXM-FRSO"
+getgenv().id = "1016006967149482054"
+_G.Script_Language = "Thai"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
