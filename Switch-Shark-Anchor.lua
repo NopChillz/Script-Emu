531bfe7354edaf79c61e4a1d422187d62b11cf68
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-01-21 22:36:24 +0700

    Add files via upload

---
+++ Switch-Shark-Anchor.lua
@@ -0,0 +1,22 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
+_G.Setting = {
+    ['FPS Booster'] = true,
+    ['White Screen'] = true,
+    ['Close Gui'] = true,
+    ['Rejoin'] = true,
+
+    ['Melee'] = 'Sharkman Karate',
+
+    ['Farm Level'] = true,
+    ['Level Max'] = 2550,
+    ['Sword Max Level'] = {
+        ['Enabled'] = true,
+        ['Sword'] = 'Shark Anchor',
+        ['Restats'] = true
+    },
+
+    ['Boat'] = 'PirateBrigade', -- เรือที่ใช้ แนะนำเรือที่เลือดเยอะๆ
+    ['Shark Anchor'] = true -- ทำสมอออโต้
+}
+script_key = "jnqUGxqGmxoCAwdLtjzFFeTGMLxroynp";
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/58328ea2259c450ea64a10414568a7dc.lua"))()
