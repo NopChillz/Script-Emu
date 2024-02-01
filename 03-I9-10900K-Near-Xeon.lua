909c33256c027e504302b8fd0aef87fe121482b8
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-01 21:42:03 +0700

    Update 03-I9-10900K-Near-Xeon.lua

--- 03-I9-10900K-Near-Xeon.lua
+++ 03-I9-10900K-Near-Xeon.lua
@@ -18,5 +18,5 @@ _G.Race_Evo = {
     ['Enabled'] = true
 }
 
-script_key="rjooxvUVqoYfytgiJSwyhVJdIkSJNsWc";
+script_key="QCHcdNiBvjjutjNdcVCCQnLktxeqjeRv";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
