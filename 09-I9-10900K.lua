c7a757a5f26775bf9619e545c56b4d3dbc0f6322
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-02 21:32:22 +0700

    Update 09-I9-10900K.lua

--- 09-I9-10900K.lua
+++ 09-I9-10900K.lua
@@ -18,5 +18,5 @@ _G.Race_Evo = {
     ['Enabled'] = true
 }
 
-script_key="OtNoTVUFXqqnRdIOrLQERSqEjyvEylzB";
+script_key="LztFLJpVyVUGRTBtIwGIgyPEeCbBVbCS";
 loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
