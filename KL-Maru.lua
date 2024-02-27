149afd7da87c147f9fca2bcd24447fac2a478c9e
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-28 01:30:17 +0700

    Update KL-Maru.lua

--- KL-Maru.lua
+++ KL-Maru.lua
@@ -30,7 +30,7 @@ _G.Quests_Settings = {
      [FarmRaid] = true
 }
 
-getgenv().Key = MARU-0D4F-3GS36-UP83-PRZXM-FRSO
-getgenv().id = 1016006967149482054
+getgenv().Key = "MARU-0D4F-3GS36-UP83-PRZXM-FRSO"
+getgenv().id = "1016006967149482054"
 getgenv().Script_Mode = Kaitun_Script
 loadstring(gameHttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
