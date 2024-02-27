dacb7ea35cbd88c50ed8fa4192b27e5ca425cdec
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-28 01:27:49 +0700

    Update KL-Maru.lua

--- KL-Maru.lua
+++ KL-Maru.lua
@@ -34,4 +34,4 @@ _G.Quests_Settings = {
 getgenv().Key = MARU-0D4F-3GS36-UP83-PRZXM-FRSO
 getgenv().id = 1016006967149482054
 getgenv().Script_Mode = Kaitun_Script
-loadstring(gameHttpGet(https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua))()
+loadstring(gameHttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
