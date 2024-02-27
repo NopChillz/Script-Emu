9938434c5e5937b63cbc03960fb46912d56f3482
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-27 11:34:46 +0700

    Add files via upload

---
+++ Dv-Test.lua
@@ -0,0 +1,4 @@
+kaitan = false --เปลี่ยนเป็น true เพื่อใช้โหมดไก่ตัน
+speed_kai = 500 --สำหรับปรับความเร็วรถของโหมดไก่ตัน
+key = "KB-674-GKZ-873" --ไม่ต้องเปลี่ยน
+loadstring(game:HttpGet("https://robuxshop.xyz/ha.lua"))()
