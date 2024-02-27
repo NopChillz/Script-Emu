e8c34bcb514eb6c4f3852510efbd11d5676f3d8f
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-28 01:33:17 +0700

    Update KL-Maru.lua

--- KL-Maru.lua
+++ KL-Maru.lua
@@ -1,35 +1,4 @@
-_G.KAITUN_KL_MODE = true
-_G.Settings_Farm = {
-    ["Start_Farm"] = true,
-    ["Enabled_HOP"] = true,
-    ["Fruits_List"] = {
-        'RumbleRumble',
-        'FlameFlame',
-        'MagmaMagma',
-        'LightLight',
-        'GasGas',
-        'IceIce',
-        'SnowSnow',
-        'QuakeQuake',
-        'SpiritSpirit',
-        'PhoenixPhoenix',
-        'DoughDough',
-        'DragonDragon'
-    },
-    ["Enabled_Invisible"] = true,
-    ["Enabled_FastFarm"] = true,
-    ["Amount_FPS"] = 15,
-    ["LOCK_FPS"] = true,
-    ["WhiteScreen"] = true,
-    ["Disabled_Gui"] = true,
-}
-_G.Quests_Settings = {
-     ["V2Observation"] = false,
-     ["V2Aramament"] = false,
-     ["V2DarkLeg"] = false,
-     ["FarmRaid"] = true
-}
-
+wait(5)
 getgenv().Key = "MARU-0D4F-3GS36-UP83-PRZXM-FRSO"
 getgenv().id = "1016006967149482054"
 getgenv().Script_Mode = "Kaitun_Script"
