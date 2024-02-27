e2a1bb3fe702e9d455d133a2304bfde475f23f41
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-02-28 01:15:13 +0700

    Add files via upload

---
+++ KL-Maru.lua
@@ -0,0 +1,37 @@
+wait(5)
+_G.KAITUN_KL_MODE = true
+_G.Settings_Farm = {
+    [Start_Farm] = true,
+    [Enabled_HOP] = true,
+    [Fruits_List] = {
+        'RumbleRumble',
+        'FlameFlame',
+        'MagmaMagma',
+        'LightLight',
+        'GasGas',
+        'IceIce',
+        'SnowSnow',
+        'QuakeQuake',
+        'SpiritSpirit',
+        'PhoenixPhoenix',
+        'DoughDough',
+        'DragonDragon'
+    },
+    [Enabled_Invisible] = true,
+    [Enabled_FastFarm] = true,
+    [Amount_FPS] = 15,
+    [LOCK_FPS] = true,
+    [WhiteScreen] = true,
+    [Disabled_Gui] = true,
+}
+_G.Quests_Settings = {
+     [V2Observation] = false,
+     [V2Aramament] = false,
+     [V2DarkLeg] = false,
+     [FarmRaid] = true
+}
+
+getgenv().Key = MARU-0D4F-3GS36-UP83-PRZXM-FRSO
+getgenv().id = 1016006967149482054
+getgenv().Script_Mode = Kaitun_Script
+loadstring(gameHttpGet(httpsraw.githubusercontent.comxshibaMaruBitkubmainMobile.lua))()
