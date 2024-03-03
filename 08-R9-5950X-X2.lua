86f02543304d2039e168bf44f863a57cd94831b0
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-03-03 15:59:40 +0700

    Update 08-R9-5950X-X2.lua

--- 08-R9-5950X-X2.lua
+++ 08-R9-5950X-X2.lua
@@ -1,22 +1,131 @@
-if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
-
-_G.Delay_Upload = 300 -- โชว์ข้อมูลใหม่ทุกๆ 300วิ
-_G.Bypass_Tp = true -- วาปฟาร์ม
-
-_G.Select_Fruit = {
-    ['Main'] = {'Kitsune-Kitsune'}, -- เลือกผลปีศาจหลัก
-    ['Select Fruit'] = {''} -- เลือกผลปีศาจ
+_G.Nexus = {
+    Main = {
+        ['Enabled'] = true,
+        ['Hop Cooldown'] = 15, -- 30 Seconds
+        ['Quests'] = {
+            ['Pull Lever'] = false,
+            ['Rainbow Haki'] = true,
+            ['Musketeer Hat'] = true,
+            ['Mirror Fractal'] = true,
+            ['Sanguine Material'] = false,
+        },
+    },
+    Sockets = {
+        Enabled = true,
+        MachineName = 'PC-08',
+        Sword = {
+            'Cursed Dual Katana',
+            'Dark Dagger',
+            'True Triple Katana',
+            'Tushita',
+            'Yama',
+            'Hallow Scythe',
+            'Saber'
+        },
+        Fruit = {
+            'Kitsune',
+            'Leopard',
+            'Dragon',
+            'Spirit',
+            'Control',
+            'Venom',
+            'Shadow',
+            'Dough',
+            'Mammoth',
+            'T-Rex'
+        }
+    },
+    Efficiency = {
+        ['FPS Booster'] = true,
+        ['Black Screen'] = false,
+    },
+    Fruits = {
+        ['Enabled'] = false,
+        ['Awakening Fruit'] = true,
+        ['Main'] = {
+            'Dough-Dough',
+        },
+        ['List'] = {
+            'Dark-Dark',
+            'Quake-Quake',
+            'Buddha-Buddha',
+            'Dark-Dark',
+            'Human-Human: Buddha',
+            'Light-Light',
+            'Dragon-Dragon',
+            'Mammoth-Mammoth',
+            'Kitsune-Kitsune',
+            'Leopard-Leopard',
+        },
+    },
+    Melee = {
+        ['6 Melee'] = true,
+    },
+    Sword = {
+        ['Pole'] = true,
+        ['Saber'] = true,
+        ['Midnight Blade'] = true,
+        ['Shisui'] = true,
+        ['Saddi'] = true,
+        ['Wando'] = true,
+        ['Yama'] = true,
+        ['Tushita'] = true,
+        ['Rengoku'] = true,
+        ['Canvander'] = true,
+        ['Buddy Sword'] = true,
+        ['Spikey Trident'] = true,
+        ['Twin Hooks'] = true,
+        ['Hallow Scryte'] = true,
+        ['True Triple Katana'] = true,
+        ['Cursed Dual Katana'] = true,
+        ['Shark Anchor'] = true,
+    },
+    Gun = {
+        ['Kabucha'] = true,
+        ['Serpent Bow'] = true,
+        ['Soul Guitar'] = true
+    },
+    Race = {
+        Enabled = true,
+        Races = {
+            ['Mink'] = true,
+            ['Human'] = false,
+            ['Fishman'] = true,
+            ['Skypiea'] = true,
+        },
+        Specials = {
+            ['Ghoul'] = false,
+            ['Cyborg'] = false,
+        },
+        V4 = {
+            ['Enabled'] = false,
+        },
+    },
+    Mastery = {
+        Sword = {
+            ['Enabled'] = true,
+            ['Manual'] = true,
+            ['Sword Name'] = {
+                'Koko',
+                'Midnight Blade',
+                'True Triple Katana',
+                'Cursed Dual Katana',
+                'Shark Anchor',
+            }
+        },
+        Gun = {
+            ['Enabled'] = false,
+            ['Manual'] = true,
+            ['Gun Name'] = {
+                'Kabucha',
+                'Acidum Rifle',
+                'Serpent Bow',
+                'Soul Guitar',
+            }
+        },
+        DevilFruits = true
+    }
 }
-_G.Mastery_Farm = { -- ฟามมาสเตอรี่
-    ['Melee'] = true,
-    ['Devil Fruit'] = true,
-    ['Sword'] = true,
-    ['Gun'] = false
-}
-_G.Race_Evo = {
-    ['Level'] = {1,2,3}, -- อีโวเผ่าจากขั้น0 ไปถึงขั้น3
-    ['Enabled'] = true
-}
 
-script_key="cPOkeSZPJprGTZohEsaLuXtntpGdJZQr";
-loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/m.lua"))()
+script_key="upHtVDywsDvBmyvDJAtZAhIcjkrTYFAw";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Xenon-Trash/Nexus/main/Nexus.lua"))()
