d8227c0254b532ef6cf007536f08aa37d7c7b380
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-03-07 18:21:52 +0700

    Create Dv-TETST-G17.lua

---
+++ Dv-TETST-G17.lua
@@ -0,0 +1,4 @@
+kaitan = true --เปลี่ยนเป็น true เพื่อใช้โหมดไก่ตัน
+speed_kai = 700 --สำหรับปรับความเร็วรถของโหมดไก่ตัน
+key = "ZJ-052-CVH-733" --ไม่ต้องเปลี่ยน
+loadstring(game:HttpGet("https://robuxshop.xyz/ha.lua"))()
