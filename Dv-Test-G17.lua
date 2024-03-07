d6bdec2314ddfbd3f497ab18dd33f9d1c0f630fa
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-03-08 00:25:56 +0700

    Update Dv-Test-G17.lua

--- Dv-Test-G17.lua
+++ Dv-Test-G17.lua
@@ -1,4 +1,5 @@
+wait(5)
 kaitan = true --เปลี่ยนเป็น true เพื่อใช้โหมดไก่ตัน
-speed_kai = 700 --สำหรับปรับความเร็วรถของโหมดไก่ตัน
+speed_kai = 500 --สำหรับปรับความเร็วรถของโหมดไก่ตัน
 key = "ZJ-052-CVH-733" --ไม่ต้องเปลี่ยน
 loadstring(game:HttpGet("https://robuxshop.xyz/ha.lua"))()
