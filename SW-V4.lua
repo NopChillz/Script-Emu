eab2b13105b54a988e24584ea22ef2988226deea
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-03-11 16:27:37 +0700

    Update SW-V4.lua

--- SW-V4.lua
+++ SW-V4.lua
@@ -1,1 +1,28 @@
+if not game:IsLoaded() then repeat game.Loaded:Wait(5) until game:IsLoaded() end
 
+_G.Setting = {
+    ['Auto Evo Race V4'] = true,
+    Race_V4 = { 
+        ['Helper'] = 'zq0vh1', 
+        ['Helper2'] = '63g6os', 
+        ['Farm'] = { 
+            'qfm03y',
+            'bdxzqv',
+			'pjbeup',
+			'dblznn',
+            'ngwo3w',
+			'cokw74',
+			'wn4s4d',
+            'o6zlyc',
+			'y3glrs',
+            'rrcbuz'
+        }
+    },
+    -- Booster CPU
+    ['Close Ui'] = true,
+    ['White Screen'] = true, 
+    ['FPS Booster'] = true
+}
+
+script_key="vOztkkXguLgknxSfgQUAkZCflTfNsTMI";
+loadstring(game:HttpGet("https://raw.githubusercontent.com/londnee/code/main/sw.v4.lua"))()
