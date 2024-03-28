5f444acd4a9ec20017c1e499b6c4ae8b3621b091
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-03-29 02:18:46 +0700

    Update Maru V4.lua

--- Maru V4.lua
+++ Maru V4.lua
@@ -2,4 +2,5 @@ wait(5)
 getgenv().Key = "MARU-IGXEM-ENGQ-GHQFG-NO9R-AUPPL"
 getgenv().id = "1220380830300635207"
 _G.Script_Language = "Thai"
+_G.FixBugLDArce = true
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xshiba/MaruBitkub/main/Mobile.lua"))()
