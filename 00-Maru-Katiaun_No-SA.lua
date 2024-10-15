50efe6214de9a057b32263a466154dfa91599d43
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-10-15 11:50:48 +0700

    Update 00-Maru-Katiaun_No-SA.lua

--- 00-Maru-Katiaun_No-SA.lua
+++ 00-Maru-Katiaun_No-SA.lua
@@ -1,4 +1,4 @@
-srepeat task.wait() until game:IsLoaded()
+repeat task.wait() until game:IsLoaded()
 repeat task.wait() until game.Players
 repeat task.wait() until game.Players.LocalPlayer
 repeat task.wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui")
