033378690ab01cac8457460d43aef2c2dca80736
NopChillz <82336260+NopChillz@users.noreply.github.com>
2024-12-29 12:08:21 +0700

    Create AA-Kaitun-Xeon.lua

---
+++ AA-Kaitun-Xeon.lua
@@ -0,0 +1,14 @@
+wait(5)
+getgenv().Configuration = {
+    ['Auto Sell'] = {
+        ['At Wave'] = 20,
+        ['Auto Leave'] = false,
+    },
+    ['RedeemCode'] = true,
+    ['Claim Quest'] = true,
+    ['Claim Battle Pass'] = true,
+};
+
+-- Discord : !OMG V4
+getgenv().key = 'c3624dba-4b52-4a1a-b287-b1d9f905e766'
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Xenon-Trash/Loader/main/Loader.lua'))()
